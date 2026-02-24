b904b2fb6473a238fbb6f08635100c544696bf29
sivavol <lim.vanessa.0106@gmail.com>
2026-02-24 13:43:51 +0700

    fix: add comment to empty setup method

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -18,6 +18,7 @@ class ProductRepositoryTest {
     ProductRepository productRepository;
     @BeforeEach
     void setUp() {
+        // Method is intentionally empty as @InjectMocks handles the initialization.
     }
     @Test
     void testCreateAndFind() {
